c2cacff4b909d15c9919fc8f7dd9e8dcb9249572
Alex Bykov <spbeat@mail.ru>
2016-05-17 17:04:25 +0300

    add Card Template
    added card to templates. More code cleanup

--- public/js/app.js
+++ public/js/app.js
@@ -1,16 +1,61 @@
-var userList = angular.module('userList', [])
-    .controller('userCtrl', function($scope, $http) {
-        $scope.userlist = [];
-        $http.get('/users/get')
-            .success(function(userlist) {
-                $scope.userlist = userlist;
-            })
-            .error(function(userlist) {
-                console.log('error occured: ' + userlist);
-            })
-    })
-    .directive('profile', function() {
-        return {
-            templateUrl: 'templates/profile.html'
-        }
-    });
+var webCourses = angular.module('webCourses', []);
+webCourses.controller('userCtrl', function($scope, $http) {
+    $scope.userlist = [];
+    $scope.coursesArray = [{
+        "name": "Javascript for dummies",
+        "desc": "Entry-level tutorials for Front-End Javascript",
+        "picture": "images/development-bg.png"
+    }, {
+        "name": "Beginner CSS",
+        "desc": "CSS From Zero to Hero",
+        "picture": "images/development-bg.png"
+    }, {
+        "name": "HTML5 Fundamentals",
+        "desc": "Leran about modern HTML",
+        "picture": "images/development-bg.png"
+    }];
+    $http.get('/users/get')
+        .success(function(userlist) {
+            $scope.userlist = userlist;
+        })
+        .error(function(userlist) {
+            console.log('error' + userlist);
+        })
+});
+
+webCourses.controller('cardCtrl', function($scope) {
+    // ЗАГЛУШКА ДЛЯ ГЕНЕРАЦИИ КУРСОВ
+    $scope.coursesArray = [{
+        "name": "Javascript for dummies",
+        "desc": "Entry-level tutorials for Front-End Javascript",
+        "picture": "images/development-bg.png"
+    }, {
+        "name": "Beginner CSS",
+        "desc": "CSS From Zero to Hero",
+        "picture": "images/development-bg.png"
+    }, {
+        "name": "HTML5 Fundamentals",
+        "desc": "Learn about modern HTML",
+        "picture": "images/development-bg.png"
+    }, {
+        "name": "Django for Perverts",
+        "desc": "How to set up a simple server in Python",
+        "picture": "images/development-bg.png"
+    }];
+});
+
+
+// =======================================
+// КАСТОМНЫЕ ДИРЕКТИВЫ. ДОБАВЛЯЙТЕ СЮДА
+webCourses.directive('card', function() {
+    // Большая карточка курса
+    return {
+        templateUrl: 'templates/card.html'
+    }
+})
+webCourses.directive('profile', function() {
+    // Список пользователей
+    return {
+        templateUrl: 'templates/profile.html'
+    }
+})
--- settings/passport.js
+++ settings/passport.js
@@ -6,8 +6,7 @@ var configAuth = require('./auth');
 
 module.exports = function(passport) {
 
-    // Модуль Стратегий авторизации. Есть локальная и фейсбук. НЕ подключены пока.
-
+    // Модуль Стратегии авторизации.
     passport.serializeUser(function(user, done) {
         done(null, user.id);
     });
@@ -18,96 +17,12 @@ module.exports = function(passport) {
         });
     });
 
-    // passport.use('local-signup', new LocalStrategy({
-    //         usernameField: 'email',
-    //         passwordField: 'password',
-    //         passReqToCallback: true
-    //     },
-    //     function(req, email, password, done) {
-    //         process.nextTick(function() {
-    //             User.findOne({
-    //                 'local.email': email
-    //             }, function(err, user) {
-    //                 if (err)
-    //                     return done(err);
-    //                 if (user) {
-    //                     return done(null, false, req.flash('signupMessage', 'That email already taken'));
-    //                 } else {
-    //                     var newUser = new User();
-    //                     newUser.local.email = email;
-    //                     newUser.local.password = password;
-    //                     newUser.save(function(err) {
-    //                         if (err)
-    //                             throw err;
-    //                         return done(null, newUser);
-    //                     })
-    //                 }
-    //             })
-    //         });
-    //     }));
-    //
-    // passport.use('local-login', new LocalStrategy({
-    //         usernameField: 'email',
-    //         passwordField: 'password',
-    //         passReqToCallback: true
-    //     },
-    //     function(req, email, password, done) {
-    //         process.nextTick(function() {
-    //             User.findOne({
-    //                 'local.email': email
-    //             }, function(err, user) {
-    //                 if (err)
-    //                     return done(err);
-    //                 if (!user)
-    //                     return done(null, false, req.flash('loginMessage', 'No User found'));
-    //                 if (user.local.password != password) {
-    //                     return done(null, false, req.flash('loginMessage', 'invalid password'));
-    //                 }
-    //                 return done(null, user);
-    //             });
-    //         });
-    //     }));
-    //
-    // passport.use(new FacebookStrategy({
-    //         clientID: configAuth.facebookAuth.clientID,
-    //         clientSecret: configAuth.facebookAuth.clientSecret,
-    //         callbackURL: configAuth.facebookAuth.callbackURL,
-    //         profileFields: ['emails' , 'name', 'id']
-    //     },
-    //     function(accessToken, refreshToken, profile, done) {
-    //         process.nextTick(function() {
-    //             User.findOne({
-    //                 'facebook.id': profile.id
-    //             }, function(err, user) {
-    //                 if (err)
-    //                     return done(err);
-    //                 if (user)
-    //                     return done(null, user);
-    //                 else {
-    //                     var newUser = new User();
-    //                     newUser.facebook.id = profile.id;
-    //                     newUser.facebook.token = accessToken;
-    //                     newUser.facebook.name = profile.name.givenName + " " + profile.name.familyName;
-    //                     newUser.facebook.email = profile.emails[0].value;
-    //
-    //                     newUser.save(function(err) {
-    //                         if (err)
-    //                             throw err;
-    //                         return done(null, newUser);
-    //                     })
-    //                 }
-    //             });
-    //         });
-    //     }
-    // ));
-
     passport.use(new GitHubStrategy({
             clientID: configAuth.githubAuth.clientID,
             clientSecret: configAuth.githubAuth.clientSecret,
             callbackURL: configAuth.githubAuth.callbackURL
         },
         function(accessToken, refreshToken, profile, done) {
-            // asynchronous verification, for effect...
             process.nextTick(function() {
                 User.findOne({
                     'github.id': profile.id
@@ -117,7 +32,6 @@ module.exports = function(passport) {
                     if (user)
                         return done(null, user);
                     else {
-                        console.log(profile); //DEV
                         var newUser = new User();
                         newUser.github.id = profile.id;
                         newUser.github.name = profile.displayName;
