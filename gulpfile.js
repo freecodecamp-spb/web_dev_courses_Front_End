8a73a4fa9d12e6504defd041501a3d81fbf908f0
Nikolay Ignatyev <noilgt@gmail.com>
2016-05-19 16:18:41 +0300

    Added gulp task
    Task for browser syncyning and live reloading after code changes

---
+++ gulpfile.js
@@ -0,0 +1,36 @@
+// Dependencies
+var gulp = require('gulp');
+var nodemon = require('gulp-nodemon');
+var browserSync = require('browser-sync').create();
+var Settings = require('./settings/settings');
+var paths = {
+    ejsFiles: "./views/**/*.ejs"
+}
+
+// Watching modified files
+gulp.task('watch', function() {
+    gulp.watch(paths.ejsFiles).on('change', browserSync.reload);
+});
+
+
+gulp.task('browser-sync', ['nodemon'], function() {
+    browserSync.init(null, {
+        proxy: "http://localhost:3000",
+        files: ["public/**/*.*"],
+        port: 7000
+    });
+});
+
+gulp.task('nodemon', function (callback) {
+    var started = false;
+    return nodemon({
+        script: 'server.js'
+    }).on('start', function () {
+        if (!started) {
+            callback();
+            started = true;
+        }
+    });
+});
+
+gulp.task('default', ['watch', 'browser-sync']);
