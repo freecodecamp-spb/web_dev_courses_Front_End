da0992d029f7b1e3ce4c062f973f00ac63d18311
tomkallen <spbeat@mail.ru>
2016-05-21 22:37:55 +0300

    fix css styles, polishing, removed fallbacks

--- public/js/app.js
+++ public/js/app.js
@@ -25,19 +25,19 @@ webCourses.controller('cardCtrl', function($scope) {
             "name": "Beginner CSS",
             "desc": "CSS From Zero to Hero",
             "picture": "images/development-bg.png",
-            "tags": ['css'],
+            "tags": ['css', 'webdesing'],
             "rating": 8.1
         }, {
             "name": "HTML5 Fundamentals",
             "desc": "Learn about modern HTML",
             "picture": "images/development-bg.png",
-            "tags": ['html'],
+            "tags": ['html', 'webdesign'],
             "rating": 6.0
         }, {
             "name": "Django for Perverts",
             "desc": "How to set up a simple server in Python",
             "picture": "images/development-bg.png",
-            "tags": ['django', 'python'],
+            "tags": ['django', 'python', 'backend'],
             "rating": 8.3
         }, {
             "name": 'Javascript for Dummies',
@@ -48,13 +48,13 @@ webCourses.controller('cardCtrl', function($scope) {
             "name": 'Front-End Bootcamp',
             "desc": 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Earum obcaecati pariatur provident praesentium atque consequatur qui quam ut nostrum minima iusto animi eligendi quaerat.',
             "picture": "images/development-bg.png",
-            "tags": ['javascript'],
+            "tags": ['javascript','frontend'],
             "rating": 7.1
         }, {
             "name": 'The CSS 3 styling guides',
             "desc": 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Earum obcaecati pariatur provident praesentium atque consequatur qui quam ut nostrum minima iusto animi eligendi quaerat.',
             "picture": "images/development-bg.png",
-            "tags": ['css'],
+            "tags": ['css', 'webdesign'],
             "rating": 5.9
         }, {
             "name": 'SVG intercourse',
@@ -66,7 +66,7 @@ webCourses.controller('cardCtrl', function($scope) {
             "name": 'HTML5: what\'s new',
             "desc": 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Earum obcaecati pariatur provident praesentium atque consequatur qui quam ut nostrum minima iusto animi eligendi quaerat.',
             "picture": "images/development-bg.png",
-            "tags": ['webdesign', 'HTML'],
+            "tags": ['webdesign', 'html'],
             "rating": 8.1
         }
     ];
