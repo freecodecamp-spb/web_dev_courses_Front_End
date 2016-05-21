0ee051d780ba250b4519e5361a97e8082c1e4db4
tomkallen <spbeat@mail.ru>
2016-05-21 20:23:35 +0300

    add flexbox cards support

--- models/usermodel.js
+++ models/usermodel.js
@@ -1,24 +1,13 @@
 var mongoose = require('mongoose');
 
 // Модель пользователя для базы
-// Фейсбук не трогайте
 var userSchema = mongoose.Schema({
-  local: {
-    email: String,
-    password: String
-  },
-  facebook: {
-    id: String,
-    token: String,
-    email: String,
-    name: String
-  },
-  github: {
-    id: String,
-    email: String,
-    name: String,
-    avatar: String
-  }
+    github: {
+        id: String,
+        email: String,
+        name: String,
+        avatar: String
+    }
 });
 
 module.exports = mongoose.model('User', userSchema);
