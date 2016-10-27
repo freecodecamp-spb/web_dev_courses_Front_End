33e9172bfd71d55f22ed5407242d163a385a0428
alexbaumgertner <alex.baumgertner@gmail.com>
2016-10-27 23:16:24 +0300

    Refactor edit form

---
+++ src/components/course-card-form/course-card-form.js
@@ -0,0 +1,150 @@
+import React, { Component } from 'react';
+import './course-card-form.css';
+
+export class CourseCardForm extends Component {
+  
+  constructor(props) {
+    super(props);
+    
+    this.state = {
+      // TODO: refactor
+      card: Object.assign({}, this.props.card)
+    };
+    
+    this.onFormChange = this.onFormChange.bind(this);
+    this.save = this.save.bind(this);
+  }
+  
+  render() {
+    
+    let tags = this.state.card.tags.map(tag => {
+      return (
+        <li
+          key={Math.random()}>
+          <input
+            className="form-control"
+            onChange={this.onFormChange}
+            value={tag}
+          />
+        </li>
+      )
+    });
+    
+    return (
+      <form
+        className="form CourseCardFrom"
+        onSubmit={(e) => { e.preventDefault() } }
+      >
+        
+        <div className="controls">
+          <button
+            className="btn btn-default"
+            onClick={this.setViewMode}>
+            View
+          </button>
+          
+          <button
+            className="btn btn-primary save"
+            onClick={this.save}>
+            Save
+          </button>
+        </div>
+        
+        <div className="header form-item">
+          <div className="input-group">
+            <label htmlFor="title" className="input-group-addon">title</label>
+            <input
+              id="title"
+              name="title"
+              className="form-control"
+              onChange={this.onFormChange}
+              value={this.state.card.title}/>
+          </div>
+        
+        </div>
+        
+        <div className="author form-item">
+          <div className="input-group">
+            <label htmlFor="author" className="input-group-addon">author</label>
+            <input
+              id="author"
+              name="author"
+              className="form-control"
+              onChange={this.onFormChange}
+              value={this.state.card.author}/>
+          </div>
+        </div>
+        
+        <div className="image form-item">
+          <div className="input-group">
+            <label htmlFor="image" className="input-group-addon">image</label>
+            <input
+              id="image"
+              name="image"
+              className="form-control"
+              onChange={this.onFormChange}
+              value={this.state.card.image}/>
+          </div>
+        </div>
+        
+        <div className="description form-item">
+          <div className="input-group">
+            <label htmlFor="description" className="input-group-addon">description</label>
+            <textarea
+              id="description"
+              name="description"
+              className="form-control"
+              rows="10"
+              onChange={this.onFormChange}
+              value={this.state.card.description}
+            />
+          </div>
+        </div>
+        
+        <div className="links form-item">
+          <div className="input-group">
+            <label htmlFor="link" className="input-group-addon">link</label>
+            <input
+              id="link"
+              name="link"
+              className="form-control"
+              onChange={this.onFormChange}
+              value={this.state.card.link}
+            />
+          </div>
+        </div>
+        
+        <div className="tags-wrapper">
+          <div>tags:</div>
+          <ul className="tags">
+            {tags}
+          </ul>
+        </div>
+      
+      </form>
+    );
+  }
+  
+  onFormChange(event) {
+    let cardEdited = {};
+    cardEdited[event.target.name] = event.target.value;
+    
+    this.setState((prevState, props) => ({
+      card: Object.assign({}, prevState.card, cardEdited)
+    }));
+  }
+  
+  save() {
+    let request = {
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      method: 'PUT',
+      body: JSON.stringify(this.state.card)
+    };
+    
+    fetch(`/api/courses/${this.props.id}`, request)
+      .then((res) => res.json())
+      .then((data) => console.log("data: ", data))
+  }
+}
--- src/components/course-card/course-card.js
+++ src/components/course-card/course-card.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import './course-card.css';
 
+import { CourseCardForm } from '../course-card-form';
+
 export class CourseCard extends Component {
   
   constructor(props) {
@@ -12,13 +14,16 @@ export class CourseCard extends Component {
     
     this.setEditMode = this.setEditMode.bind(this);
     this.setViewMode = this.setViewMode.bind(this);
-    this.onFormChange = this.onFormChange.bind(this);
-    this.save = this.save.bind(this);
   }
   
   render() {
     if (this.state.isEditMode) {
-      return this.getEditLayout();
+      return (
+        <CourseCardForm
+          id={this.props.id}
+          card={this.props.card}
+        />
+      );
     } else {
       return this.getViewLayout(this.props.card);
     }
@@ -84,137 +89,12 @@ export class CourseCard extends Component {
       </div>
     );
   }
-  
-  getEditLayout() {
-    if (!this.state.card) {
-      return (
-        <div>Loading...</div>
-      );
-    }
-    
-    let tags = this.state.card.tags.map(tag => {
-      return (
-        <li
-          key={Math.random()}>
-          <input
-            className="form-control"
-            onChange={this.onFormChange}
-            value={tag}
-          />
-        </li>
-      )
-    });
-    
-    return (
-      <form
-        className="form CourseCard"
-        onSubmit={(e) => { e.preventDefault() } }
-      >
-  
-        <div className="controls">
-          <button
-            className="btn btn-default"
-            onClick={this.setViewMode}>
-            View
-          </button>
-  
-          <button
-            className="btn btn-primary save"
-            onClick={this.save}>
-            Save
-          </button>
-        </div>
-        
-        <div className="header form-item">
-          <div className="input-group">
-            <label htmlFor="title" className="input-group-addon">title</label>
-            <input
-              id="title"
-              name="title"
-              className="form-control"
-              onChange={this.onFormChange}
-              value={this.state.card.title}/>
-          </div>
-        
-        </div>
-        
-        <div className="author form-item">
-          <div className="input-group">
-            <label htmlFor="author" className="input-group-addon">author</label>
-            <input
-              id="author"
-              name="author"
-              className="form-control"
-              onChange={this.onFormChange}
-              value={this.state.card.author}/>
-          </div>
-        </div>
-        
-        <div className="image form-item">
-          <div className="input-group">
-            <label htmlFor="image" className="input-group-addon">image</label>
-            <input
-              id="image"
-              name="image"
-              className="form-control"
-              onChange={this.onFormChange}
-              value={this.state.card.image}/>
-          </div>
-        </div>
-        
-        <div className="description form-item">
-          <div className="input-group">
-            <label htmlFor="description" className="input-group-addon">description</label>
-            <textarea
-              id="description"
-              name="description"
-              className="form-control"
-              rows="10"
-              onChange={this.onFormChange}
-              value={this.state.card.description}
-            />
-          </div>
-        </div>
-        
-        <div className="links form-item">
-          <div className="input-group">
-            <label htmlFor="link" className="input-group-addon">link</label>
-            <input
-              id="link"
-              name="link"
-              className="form-control"
-              onChange={this.onFormChange}
-              value={this.state.card.link}
-            />
-          </div>
-        </div>
-  
-        <div className="tags-wrapper">
-          <div>tags:</div>
-          <ul className="tags">
-            {tags}
-          </ul>
-        </div>
-        
-      </form>
-    );
-  }
-  
-  onFormChange(event) {
-    let cardEdited = {};
-    cardEdited[event.target.name] = event.target.value;
-  
-    this.setState((prevState, props) => ({
-      card: Object.assign({}, prevState.card, cardEdited)
-    }));
-  }
-  
+
   setEditMode(e) {
     e.preventDefault();
     
     this.setState({
-      isEditMode: true,
-      card: this.props.card
+      isEditMode: true
     });
   }
   
@@ -224,20 +104,5 @@ export class CourseCard extends Component {
     this.setState({
       isEditMode: false
     });
-  }
-  
-  save() {
-    let request =  {
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      method: 'PUT',
-      body: JSON.stringify(this.state.card)
-    };
-    
-  
-    fetch(`/api/courses/${this.props.id}`, request)
-    .then((res) => res.json())
-    .then((data) => console.log("data: ", data))
   }
 }
