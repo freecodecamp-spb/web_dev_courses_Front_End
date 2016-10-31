8729b04c98d15bbdcd4ff892db18dfb574510c23
alexbaumgertner <alex.baumgertner@gmail.com>
2016-11-01 01:07:59 +0300

    [WIP] Add login API

--- src/index.js
+++ src/index.js
@@ -11,6 +11,8 @@ import {
 import './index.css';
 
 import App from './App';
+import { AuthPage } from './pages/auth/auth';
+
 import { CoursesListPage } from './pages/courses-list/couses-list';
 import { CoursesItemPage } from './pages/courses-item/courses-item';
 import { CoursesNewPage } from './pages/courses-new/courses-new';
@@ -20,8 +22,8 @@ ReactDOM.render((
       <Route path="/" component={App}>
 
         <IndexRedirect to="/courses/" />
-        
-        <Route path="/courses/" component={CoursesListPage} />
+
+        <Route path="/courses" component={CoursesListPage} />
         <Route path="/courses/new" component={CoursesNewPage}/>
         <Route path="/courses/:id" component={CoursesItemPage}/>
       
