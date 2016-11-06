0a2e24f3fac3d051811eef85549ac35ebb437d03
alexbaumgertner <alex.baumgertner@gmail.com>
2016-11-06 16:41:14 +0300

    Delete init code (mount App)

--- src/index.js
+++ src/index.js
@@ -15,11 +15,6 @@ import { CoursesListPage } from './pages/courses-list/couses-list';
 import { CoursesItemPage } from './pages/courses-item/courses-item';
 import { CoursesNewPage } from './pages/courses-new/courses-new';
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
-
 ReactDOM.render((
     <Router history={browserHistory}>
       <Route path="/" component={App}>
