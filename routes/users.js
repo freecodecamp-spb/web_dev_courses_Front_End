b75fa72644abcdd6048c4a974dc36d5f09a7b478
alexbaumgertner <alex.baumgertner@gmail.com>
2016-11-19 23:41:50 +0300

    Add auth scaffold description

--- routes/users.js
+++ routes/users.js
@@ -2,64 +2,34 @@ const express = require('express');
 const User = require('../models/usermodel');
 
 module.exports = function(app, passport) {
-  app.get('/profile', function(req, res) {
-    if (req.user) {
-      res.render("index", {
-        user: req.user
-      });
-    } else {
-      res.redirect("/");
-    }
+
+  app.post('/api/auth/login', (req, res) => {
+    // Проверить, есть ли такой пользователь в БД и
+    // совпадает ли зашифрованный пароль в запросе и в БД
+
+    // если совпадают - создать нового пользователя в бд и
+    // создать сессию и установить сессионную cookie пользователю (TODO: перейти на JWT)
+
+    // если не совпадают — вернуть ошибку и предложить напомнить пароль
   });
 
-  app.get(
-    '/api/auth/github',
-    (req, res, next) => {
-      passport.authenticate('github', {scope: ['user']}, (error, user, info) => {
-        console.log("error: ", error);
-        console.log("user: ", user);
-        console.log("info: ", info);
-      });
-    }
-    /*
-    *
-    *  https://github.com/login/oauth/authorize?
-    *  response_type=code&
-    *  redirect_uri=ht…A3333%2Fauth%2Fgithub%2Fcallback&
-    *  scope=user&
-    *  client_id=a7d4334f13bbacdd7303.
-    *
-    *  Redirect from 'https://github.com/login/oauth/authorize?response_type=code&redirect_uri=ht…A3333%2Fauth%2Fgithub%2Fcallback&scope=user&client_id=a7d4334f13bbacdd7303
-    *
-    *
-    *
-    *
-    * */
+  app.post('/api/auth/signup', (req, res) => {
+    // Проверить, есть ли такой пользователь в БД,
+    // если есть — вернуть ошибку и предложить войти/напомнить пароль
 
-  );
+    // если пользователя в БД нет,
+    // проверить совпадает ли пароль и подтверждение пароля
+    // если не совпапают — вернуть ошибку и предложить ввести пароли заново
 
-  app.get('/auth/github/callback',
-    passport.authenticate('github', {
-      successRedirect: '/',
-      failureRedirect: '/'
-    })
-  );
-
-  app.get('/logout', function(req, res) {
-    req.logout();
-    res.redirect('/');
+    // если совпадают - создать нового пользователя в бд и
+    // создать сессию и установить сессионную cookie пользователю (TODO: перейти на JWT)
   });
 
-  app.get('/users', function(req, res) {
-    res.render('index', {
-      user: req.user
-    });
-  });
-
-  app.get('/users/get', function(req, res) {
+  app.get('/api/users/', function(req, res) {
     User.find(function(err, userlist) {
       if (err) res.send(err);
       res.json(userlist);
     });
   });
+
 };
