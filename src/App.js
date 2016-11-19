9fde1b42ca30c24a6db8dc0c68ff14099788c843
alexbaumgertner <alex.baumgertner@gmail.com>
2016-11-19 23:28:48 +0300

    Add local login form

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router';
+
 import './App.css';
 
 class App extends Component {
@@ -6,6 +8,9 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <div className="App-login">
+          <Link to="/login">Login</Link>
+        </div>
         <div className="App-header">
           <h2>Это каталог курсов по веб-разработке!</h2>
         </div>
