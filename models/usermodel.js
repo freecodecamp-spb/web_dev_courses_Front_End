d5401494221cfd0e95c24aa378bd25b588595a66
alexbaumgertner <alex.baumgertner@gmail.com>
2016-11-23 22:29:35 +0300

    Fix User signup

--- models/usermodel.js
+++ models/usermodel.js
@@ -2,12 +2,14 @@ const mongoose = require('mongoose');
 
 // Модель пользователя для базы
 const userSchema = mongoose.Schema({
-    github: {
-        id: String,
-        email: String,
-        name: String,
-        avatar: String
-    }
+  email: String,
+  password: String,
+  github: {
+    id: String,
+    email: String,
+    name: String,
+    avatar: String
+  }
 });
 
 module.exports = mongoose.model('User', userSchema);
