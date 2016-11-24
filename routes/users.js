30378c2438cf7a79584639de416133fd3f913cad
alexbaumgertner <alex.baumgertner@gmail.com>
2016-11-24 23:33:34 +0300

    Fix User login

--- routes/users.js
+++ routes/users.js
@@ -5,6 +5,38 @@ const bcrypt = require('bcrypt');
 module.exports = function(app, passport) {
 
   app.post('/api/auth/login', (req, res) => {
+    let userData = req.body || {};
+
+    // Проверить, есть ли такой пользователь в БД и
+    User.findOne({
+      'email': userData.email
+    }, (err, data) => {
+
+      if (err) {
+        res.status('400').json({
+          error: err
+        });
+      } else if (data) {
+
+        if (bcrypt.compareSync(userData.password, data.password)) {
+          res.status('201').json({
+            success: 'User authenticated',
+            data
+          });
+        } else {
+          res.status('401').json({
+            error: 'Unauthorized',
+            data
+          });
+        }
+
+      } else {
+        res.status('400').json({
+          error: 'Something wrong'
+        });
+      }
+
+    });
     // Проверить, есть ли такой пользователь в БД и
     // совпадает ли зашифрованный пароль в запросе и в БД
 
@@ -20,56 +52,56 @@ module.exports = function(app, passport) {
 
     // Проверить, есть ли такой пользователь в БД,
     User.findOne({
-        'email': userData.email
-      }, (err, data) => {
-        // ошибка в БД
-        if (err) {
+      'email': userData.email
+    }, (err, data) => {
+      // ошибка в БД
+      if (err) {
+        res.status('400').json({
+          error: err
+        });
+
+        // Пользователь уже существует
+      } else if (data) {
+        // если есть — вернуть ошибку и предложить войти/напомнить пароль
+        res.status('409').json({
+          error: 'User already exists',
+          data
+        });
+
+      } else {
+        // если пользователя в БД нет,
+
+        if (userData.password !== userData.passwordConfirm) {
           res.status('400').json({
-            error: err
+            error: 'Password confirmation error'
           });
-
-          // Пользователь уже существует
-        } else if (data) {
-          // если есть — вернуть ошибку и предложить войти/напомнить пароль
-          res.status('409').json({
-            error: 'User already exists',
-            data
-          });
-
         } else {
-          // если пользователя в БД нет,
+          delete userData.passwordConfirm;
 
-          if (userData.password !== userData.passwordConfirm) {
-            res.status('400').json({
-              error: 'Password confirmation error'
-            });
-          } else  {
-            delete userData.passwordConfirm;
+          userData.password = bcrypt.hashSync(userData.password, 10);
 
-            userData.password = bcrypt.hashSync(userData.password, 10);
+          userModel = new User(userData);
 
-            userModel = new User(userData);
+          // проверить совпадает ли пароль и подтверждение пароля
+          // если не совпапают — вернуть ошибку и предложить ввести пароли заново
 
-            // проверить совпадает ли пароль и подтверждение пароля
-            // если не совпапают — вернуть ошибку и предложить ввести пароли заново
+          userModel.save((err, data) => {
+            if (err) {
+              res.status('400').json({
+                error: err
+              });
 
-            userModel.save((err, data) => {
-              if (err) {
-                res.status('400').json({
-                  error: err
-                });
+            } else {
+              res.status('201').json({
+                success: 'User created',
+                data
+              });
+            }
 
-              } else {
-                res.status('201').json({
-                  success: 'User created',
-                  data
-                });
-              }
-
-            });
-          }
+          });
         }
-      });
+      }
+    });
 
     // если совпадают - создать нового пользователя в бд и
     // создать сессию и установить сессионную cookie пользователю (TODO: перейти на JWT)
