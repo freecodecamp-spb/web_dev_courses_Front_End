add559382a630547d60d1d8a2de20649a5f1abc3
tomkallen <spbeat@mail.ru>
2016-12-19 00:20:24 +0300

    Removed unneeded router assignment

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ const morgan = require('morgan');
 const session = require('express-session');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+
+// Подключение собственных модулей
+
 const Settings = require('./settings'); //настройки сервера
 
 const app = express(); //инициализация сервера на Express
