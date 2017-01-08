30fe80e7f0e0fada6ab96e5554e42f9bc949ded1
alexbaumgertner <alex.baumgertner@gmail.com>
2017-01-08 14:23:32 +0300

    Add main menu

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router';
 
 import { Userbar } from './components/userbar';
 
@@ -14,6 +15,12 @@ class App extends Component {
         <div className="App-header">
           <h2>web | dev | courses</h2>
         </div>
+        <nav className="App-memu">
+          <ul>
+            <li><Link to="/">Главная</Link></li>
+            <li><Link to="/courses">Каталог</Link></li>
+          </ul>
+        </nav>
         {this.props.children}
       </div>
     );
